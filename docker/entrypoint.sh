32301f8f967e90030fd9206745e075ad7de9070c
liuzhenwei <dui_zhang@163.com>
2025-05-26 14:31:38 +0800

    add test user

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,6 +13,7 @@ OSS_BUCKET=${OSS_REGION}
 EOF
 
 psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -W ${POSTGRES_PASSWORD}  -h postgres -c "CREATE EXTENSION IF NOT EXISTS vector;"
+psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -W ${POSTGRES_PASSWORD}  -h postgres -f ./prisma/add-users.sql
 
 echo "start server..."
 npm run start
