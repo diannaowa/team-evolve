e78ea2a6c8cfdea595aaf158860137d895c99c47
liuzhenwei <dui_zhang@163.com>
2025-05-26 12:52:53 +0800

    fix psql client host

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,7 +11,7 @@ OSS_ACCESS_KEY_SECRET=${OSS_REGION}
 OSS_BUCKET=${OSS_REGION}
 EOF
 
-psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -h redis -c "CREATE EXTENSION vector;"
+psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -h postgres -c "CREATE EXTENSION vector;"
 
 echo "start server..."
 npm run start
